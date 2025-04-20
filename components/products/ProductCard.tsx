cdc6b1081e0d0b176b870e0bb870d802aa520e3a
afgomezv <afgomezvonline@gmail.com>
2025-04-20 11:04:34 -0500

    create product

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { formatCurrency } from "@/src/utils";
+import { formatCurrency, getImagePath } from "@/src/utils";
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import AddProductButton from "./AddProductButton";
@@ -8,22 +8,26 @@ type ProductCardProps = {
 };
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const imagePath = getImagePath(product.image);
   return (
-    <div className="border border-gray-300 bg-white">
+    <div className="flex flex-col h-full border border-gray-300 bg-white">
       <Image
         width={400}
         height={500}
-        src={`/products/${product.image}.jpg`}
+        src={imagePath}
         alt={`Imagen platillo ${product.name}`}
+        className="object-cover h-64 w-full"
         // quality={75} default
       />
-      <div className="p-5">
-        <h3 className="text-2xl font-bold">{product.name}</h3>
-        <p className="mt-5 font-black text-4xl text-amber-500">
-          {formatCurrency(product.price)}
-        </p>
+      <div className="flex flex-col justify-between flex-1 p-5">
+        <div>
+          <h3 className="text-2xl font-bold">{product.name}</h3>
+          <p className="mt-5 font-black text-4xl text-amber-500">
+            {formatCurrency(product.price)}
+          </p>
+        </div>
+        <AddProductButton product={product} />
       </div>
-      <AddProductButton product={product} />
     </div>
   );
 }
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -24,3 +24,23 @@ export const OrderIdSchema = z.object({
 export const SearchSchema = z.object({
   search: z.string().trim().min(1, "La búsqueda es obligatoria"),
 });
+
+export const ProductSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: "El nombre del producto es obligatorio" }),
+  price: z
+    .string()
+    .trim()
+    .transform((value) => parseFloat(value))
+    .refine((value) => value > 0, { message: "El precio no es  válido" })
+    .or(z.number().min(1, { message: "La categoría es obligatoria" })),
+  categoryId: z
+    .string()
+    .trim()
+    .transform((value) => parseInt(value))
+    .refine((value) => value > 0, { message: "La categoría es obligatoria" })
+    .or(z.number().min(1, { message: "La categoría es obligatoria" })),
+  image: z.string().min(1, { message: "La imagen es obligatoria" }),
+});
