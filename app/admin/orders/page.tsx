be043420c512f2d1cb93bcd1bb9172ed10c66488
afgomezv <afgomezvonline@gmail.com>
2025-04-20 13:35:49 -0500

    new orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,38 +1,48 @@
+"use client";
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from "@/src/types";
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
+export default function OrdersPage() {
+  const url = "/admin/orders/api";
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 3600000,
+    revalidateOnFocus: false,
   });
-  return orders;
-}
 
-export default async function OrdersPage() {
-  const orders = await getPendingOrders();
+  // const refreshOrders = async () => {
+  //   "use server";
+  //   revalidatePath("/admin/orders");
+  // };
+  if (isLoading) return "Cargando..";
 
-  return (
-    <>
-      <Heading>Administrar Ordenes</Heading>
-      {orders.length ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
-            <OrderCard key={order.id} order={order} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center">No hay ordenes pendientes</p>
-      )}
-    </>
-  );
+  if (data)
+    return (
+      <>
+        <Heading>Administrar Ordenes</Heading>
+        {/* <form action={refreshOrders}>
+        <button
+          type="submit"
+          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+        >
+          Actualizar Ordenes
+        </button>
+      </form> */}
+        {data.length ? (
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5 auto-rows-fr">
+            {data.map((order) => (
+              <OrderCard key={order.id} order={order} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No hay ordenes pendientes</p>
+        )}
+      </>
+    );
 }
--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -10,27 +10,32 @@ export default function OrderCard({ order }: OrderCardProps) {
   return (
     <section
       aria-labelledby="summary-heading"
-      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
+      className="flex flex-col justify-between h-full rounded-lg bg-gray-50 shadow-xl px-4 py-6 sm:p-6 lg:p-8"
     >
-      <p className="text-2xl font-medium text-gray-900">
-        Cliente: {order.name}{" "}
-      </p>
-      <p className="text-lg font-medium text-gray-900">Productos Ordenados:</p>
-      <dl className="mt-6 space-y-4">
-        {order.orderProducts.map((product) => (
-          <div
-            key={product.id}
-            className="flex items-center gap-2 border-t border-gray-200 pt-4"
-          >
-            <dt className="flex items-center text-sm text-gray-600">
-              <span className="font-black">({product.quantity}) </span>
-            </dt>
-            <dd className="text-sm font-medium text-gray-900">
-              {product.product.name}
-            </dd>
-          </div>
-        ))}
-        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+      <div>
+        <p className="text-2xl font-medium text-gray-900">
+          Cliente: {order.name}
+        </p>
+        <p className="text-lg font-medium text-gray-900 mt-2">
+          Productos Ordenados:
+        </p>
+
+        <dl className="mt-4 space-y-4 max-h-52 overflow-y-auto pr-2">
+          {order.orderProducts.map((product) => (
+            <div
+              key={product.id}
+              className="flex items-center gap-2 border-t border-gray-200 pt-4"
+            >
+              <dt className="flex items-center text-sm text-gray-600">
+                <span className="font-black">({product.quantity}) </span>
+              </dt>
+              <dd className="text-sm font-medium text-gray-900">
+                {product.product.name}
+              </dd>
+            </div>
+          ))}
+        </dl>
+        <div className="flex items-center justify-between border-t border-gray-200 pt-4 mt-4">
           <dt className="text-base font-medium text-gray-900">
             Total a Pagar:
           </dt>
@@ -38,7 +43,7 @@ export default function OrderCard({ order }: OrderCardProps) {
             {formatCurrency(order.total)}
           </dd>
         </div>
-      </dl>
+      </div>
 
       <form action={completeOrder}>
         <input type="hidden" value={order.id} name="order_id" />
